1872ee1fcddaf3b3a46f6b784636292ff441719f
zhangliao <zhangliao@baiwang.com>
2021-12-09 15:30:08 +0800

    feat: 递归实现全排列

---
+++ src/permutations.mjs
@@ -0,0 +1,26 @@
+export function permutations(baseList){
+    if(!Array.isArray(baseList)){
+        return
+    }
+
+    if(baseList.length === 1){
+        return [baseList];
+    }
+
+    const lessPermutation = permutations(baseList.slice(1))
+    const firstEl = baseList[0]
+
+    const resultList = []
+    lessPermutation.forEach(lessPermuItem => {
+        // n + 1 个可插入的位置
+        for(let i = 0, len = lessPermuItem.length; i <= len; i++){
+            const tempResult = [...lessPermuItem]
+            tempResult.splice(i,0, firstEl)
+            resultList.push(tempResult)
+        }
+    })
+    return resultList
+}
+
+console.log(permutations([1,2,3,4,5,6]))
+console.log(permutations(['A','B','C','D','E','F']))
