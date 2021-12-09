e12f6775922950ddea6e4e86308ca4adfe4dbdc2
zhangliao <zhangliao@baiwang.com>
2021-12-09 09:45:39 +0800

    feat: 实现字典序后继方法生成R子集

---
+++ src/combinations-2.mjs
@@ -0,0 +1,65 @@
+
+// 获取最大的K，也就是要变化的位置
+// a[k] + 1 <= maxEl, a[k] 不在 prevList中
+const getMaxKPosition = (prevList, maxEl, subSetLen) => {
+    let maxK = subSetLen
+    let maxKValue = prevList[maxK - 1] + 1
+    while (prevList.includes(maxKValue) || maxKValue > maxEl){
+        maxK--
+        maxKValue = prevList[maxK - 1] + 1
+    }
+    return maxK
+}
+
+const getNextCombination = (prevList,maxK,subSetLen) => {
+    let nextList = [...prevList]
+    if(maxK === subSetLen){
+        nextList[maxK - 1] = nextList[maxK - 1] + 1
+    }else{
+        nextList[maxK - 1] = nextList[maxK - 1] + 1
+        for(let i = maxK; i < subSetLen; i++){
+            nextList[i] = nextList[i - 1] + 1
+        }
+    }
+    return nextList
+}
+
+function combinationsDict(baseLen, subSetLen){
+    if(baseLen < subSetLen){
+        return
+    }
+
+    const baseList = []
+    for(let i = 1; i <= baseLen; i++){
+        baseList.push(i)
+    }
+
+    let prevList = baseList.slice(0,subSetLen)
+    let lastList = baseList.slice(baseLen - subSetLen)
+
+    const resultList = [
+        prevList
+    ]
+    let maxEl = baseList[baseLen - 1]
+    let maxK = getMaxKPosition(prevList,maxEl,subSetLen)
+    while (prevList.join() !== lastList.join()){
+        prevList = getNextCombination(prevList,maxK,subSetLen)
+        resultList.push(prevList)
+        maxK = getMaxKPosition(prevList,maxEl,subSetLen)
+    }
+    return resultList
+}
+
+
+export function combinations(baseList, subSetLen){
+    const indicateList = combinationsDict(baseList.length,subSetLen)
+    return indicateList.map((itemList,itemListIndex) => {
+        return itemList.map((item,index) => {
+            return baseList[item - 1]
+        })
+    })
+}
+
+console.log(combinationsDict(6,3))
+console.log(combinations([1,2,3,4,5,6],3))
+console.log(combinations(['A','B','C','D','E','F'],3))
